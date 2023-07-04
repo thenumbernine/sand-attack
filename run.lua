cf6a9237eb4ef12d6d5d12c2aa161e296161a91f
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-04 22:37:47 +0700

    more

--- run.lua
+++ run.lua
@@ -10,10 +10,23 @@ local GLTex2D = require 'gl.tex2d'
 local GLProgram  = require 'gl.program'
 local glreport = require 'gl.report'
 local vec2i = require 'vec-ffi.vec2i'
+local vec3f = require 'vec-ffi.vec3f'
 local getTime = require 'ext.timer'.getTime
 local App = require 'imguiapp.withorbit'()
-	
-local pieceSize = vec2i(40,40)
+
+-- board size is 80 x 144 visible
+-- piece is 4 blocks arranged
+-- blocks are 8 x 8
+local voxelsPerBlock = 8
+local pieceSizeInBlocks = vec2i(4,4)
+local pieceSize = pieceSizeInBlocks * voxelsPerBlock
+
+local updateInterval = 1/60
+--local updateInterval = 1/120
+--local updateInterval = 0
+
+local toppleChance = .5
+local ticksToFall = 5
 
 App.title = 'Sand Tetris'
 
@@ -29,13 +42,13 @@ function App:initGL(...)
 	self.view.orbit:set(.5, .5, 0)
 	self.view.pos:set(.5, .5, 10)
 
-	-- board size is 80 x 144 visible
-	-- piece is 4 blocks arranged
-	-- blocks are 8 x 8
-
+	-- original:
 	self.sandSize = vec2i(80, 144)
+	-- meh
+	--self.sandSize = vec2i(160, 288)
+	
 	self.sandVolume = tonumber(self.sandSize.x * self.sandSize.y)
-	self.sandCPU = ffi.new('int[?]', self.sandVolume)
+	self.sandImage = Image(self.sandSize.x, self.sandSize.y, 4, 'unsigned char')
 	self.sandTex = GLTex2D{
 		internalFormat = gl.GL_RGBA,
 		width = tonumber(self.sandSize.x),
@@ -74,12 +87,12 @@ local function makePieceImage(s)
 	s = string.split(s, '\n')
 	local img = Image(pieceSize.x, pieceSize.y, 4, 'unsigned char')
 	ffi.fill(img.buffer, 4 * img.width * img.height)
-	for j=0,3 do
-		for i=0,3 do
+	for j=0,pieceSizeInBlocks.y-1 do
+		for i=0,pieceSizeInBlocks.x-1 do
 			if s[j+1]:sub(i+1,i+1) == '#' then
-				for u=0,7 do
-					for v=0,7 do
-						ffi.cast('int*', img.buffer)[(u + 8 * i) + img.width * (v + 8 * j)] = -1
+				for u=0,voxelsPerBlock-1 do
+					for v=0,voxelsPerBlock-1 do
+						ffi.cast('int*', img.buffer)[(u + voxelsPerBlock * i) + img.width * (v + voxelsPerBlock * j)] = -1
 					end
 				end
 			end
@@ -134,57 +147,80 @@ local pieceImages = table{
 }
 
 local colors = table{
-	0xff0000,
-	0x00ff00,
-	0xffff00,
-	0x0000ff,
+	vec3f(1,0,0),
+	vec3f(1,1,0),
+	vec3f(0,1,0),
+	vec3f(0,0,1),
 }
 
 function App:reset()
 	local w, h = self.sandSize:unpack()
-	ffi.fill(self.sandCPU, ffi.sizeof'int' * w * h)
+	ffi.fill(self.sandImage.buffer, 4 * w * h)
 	self.sandTex
 		:bind()
-		:subimage{data=self.sandCPU}	--a case for saving .data ? like I do with gl buffers?
+		:subimage{data=self.sandImage.buffer}	--a case for saving .data ? like I do with gl buffers?
 		:unbind()
 
 	self:newPiece()
-	
+
 	self.lastUpdateTime = getTime()
 end
 
 function App:newPiece()
 	local w, h = self.sandSize:unpack()
 	local srcimage = pieceImages:pickRandom()
-	--[[
-	ffi.copy(self.pieceImage.buffer, srcimage.buffer, pieceSize.x*pieceSize.y*4)
-	--]]
-	-- [[
+	local color = colors:pickRandom()	
 	local srcp = ffi.cast('uint32_t*', srcimage.buffer)
 	local dstp = ffi.cast('uint32_t*', self.pieceImage.buffer)
 	for j=0,pieceSize.y-1 do
 		for i=0,pieceSize.x-1 do
 			local k = i + pieceSize.x * j
 			if srcp[0] ~= 0 then
-				dstp[0] = math.random(0,16777215)
+				--local l = math.random() * .5 + .5
+				local l = 1	-- for now
+				dstp[0] = bit.bor(
+					math.floor(l * color.x * 255),
+					bit.lshift(math.floor(l * color.y * 255), 8),
+					bit.lshift(math.floor(l * color.z * 255), 16),
+					0xff000000
+				)
 			else
-				dstp[0] = math.random(0,16777215)
+				dstp[0] = 0
 			end
-			dstp[0] = bit.bor(dstp[0], 0xff000000)
 			srcp = srcp + 1
 			dstp = dstp + 1
 		end
 	end
 	--]]
 	self:updatePieceTex()
-	self.piecePos = vec2i(bit.rshift(w,1), h-1)
-	self.pieceColor = colors:pickRandom()	
+	self.piecePos = vec2i(bit.rshift(w-pieceSize.x,1), h-1)
 	if self:testPieceMerge() then
 		print("YOU LOSE!!!")
 	end
+	self.fallTick = 0 
 end
 
 function App:updatePieceTex()
+	-- while we're here, find the first and last cols with content
+	for _,info in ipairs{
+		{0,pieceSize.x-1,1, 'pieceColMin'},
+		{pieceSize.x-1,0,-1, 'pieceColMax'},
+	} do
+		local istart, iend, istep, ifield = table.unpack(info)
+		for i=istart,iend,istep do
+			local found
+			for j=0,pieceSize.y-1 do
+				if ffi.cast('int*', self.pieceImage.buffer)[i + pieceSize.x * j] ~= 0 then
+					found = true
+					break
+				end
+			end
+			if found then
+				self[ifield] = i
+				break
+			end
+		end
+	end
 	self.pieceTex:bind()
 		:subimage{data=self.pieceImage.buffer}
 end
@@ -196,7 +232,8 @@ function App:rotatePiece()
 	for j=0,pieceSize.x-1 do
 		for i=0,pieceSize.y-1 do
 			for ch=0,3 do
-				self.pieceImage2.buffer[ch + 4 * (i + pieceSize.x * j)] = self.pieceImage.buffer[ch + 4 * ((pieceSize.x - 1 - j) + pieceSize.x * i)]
+				self.pieceImage2.buffer[ch + 4 * (i + pieceSize.x * j)] 
+				= self.pieceImage.buffer[ch + 4 * ((pieceSize.x - 1 - j) + pieceSize.x * i)]
 			end
 		end
 	end
@@ -212,9 +249,6 @@ local vtxs = {
 	{0,1},
 }
 
-local updateInterval = 1/60
---local updateInterval = 0
-
 function App:testPieceMerge()
 	local w, h = self.sandSize:unpack()
 	for j=0,pieceSize.y-1 do
@@ -226,7 +260,7 @@ function App:testPieceMerge()
 				local y = self.piecePos.y + j
 				if x >= 0 and x < w
 				and y >= 0 and y < h
-				and self.sandCPU[x + w * y] ~= 0
+				and ffi.cast('int*',self.sandImage.buffer)[x + w * y] ~= 0
 				then
 					return true
 				end
@@ -244,7 +278,7 @@ function App:updateGame()
 	self.lastUpdateTime = self.lastUpdateTime + updateInterval
 
 	-- update
-	local prow = self.sandCPU + w
+	local prow = ffi.cast('int*', self.sandImage.buffer) + w
 	for j=1,h-1 do
 		-- 50/50 cycling left-to-right vs right-to-left
 		local istart, iend, istep
@@ -260,7 +294,7 @@ function App:updateGame()
 				if p[-w] == 0 then
 					p[0], p[-w] = p[-w], p[0]
 				-- hmm symmetry? check left vs right first?
-				else
+				elseif math.random() < toppleChance then
 					-- 50/50 check left then right, vs check right then left
 					if math.random(2) == 2 then
 						if i > 0 and p[-w-1] == 0 then
@@ -285,20 +319,30 @@ function App:updateGame()
 	-- now draw the shape over the sand
 	-- test piece for collision with sand
 	-- if it collides then merge it
-	local merge
-	self.piecePos.y = self.piecePos.y - 1
+	local movedx = 1
+	local movedy = 3
 	-- TODO key updates at higher interval than drop rate ...
 	-- but test collision for both
 	if self.leftPress then
-		self.piecePos.x = self.piecePos.x - 1
+		self.piecePos.x = self.piecePos.x - movedx
 		-- TODO check blit and don't move if any pixels are oob
-		if self.piecePos.x < 0 then self.piecePos.x = 0 end
+		if self.piecePos.x < -self.pieceColMin then self.piecePos.x = -self.pieceColMin end
 	end
 	if self.rightPress then 
-		self.piecePos.x = self.piecePos.x + 1 
-		if self.piecePos.x > w-pieceSize.x-1 then self.piecePos.x = w-pieceSize.x-1 end
+		self.piecePos.x = self.piecePos.x + movedx 
+		if self.piecePos.x > w-1-self.pieceColMax then
+			self.piecePos.x = w-1-self.pieceColMax
+		end
 	end
-	if self.downPress then self.piecePos.y = self.piecePos.y - 1 end
+	if self.downPress then self.piecePos.y = self.piecePos.y - movedy end
+	
+	self.fallTick = self.fallTick + 1
+	if self.fallTick >= ticksToFall then
+		self.fallTick = 0
+		self.piecePos.y = self.piecePos.y - movedx
+	end
+
+	local merge
 	if self.piecePos.y <= 0 then
 		self.piecePos.y = 0
 		merge = true
@@ -315,9 +359,9 @@ function App:updateGame()
 					local y = self.piecePos.y + j
 					if x >= 0 and x < w
 					and y >= 0 and y < h
-					and self.sandCPU[x + w * y] == 0
+					and ffi.cast('int*', self.sandImage.buffer)[x + w * y] == 0
 					then
-						self.sandCPU[x + w * y] = color
+						ffi.cast('int*', self.sandImage.buffer)[x + w * y] = color
 					end
 				end
 			end
@@ -325,6 +369,27 @@ function App:updateGame()
 	
 		self:newPiece()
 	end
+
+	--[[ now ... try to find a connection from left to right
+	local function checkNextCol(i, jmin, jmax, color)
+	
+	end
+	local j = 0
+	while j < h-1 do
+		local color = self.sandCPU[0 + w * j]
+		if color ~= 0 then
+			-- this is the start of our color interval
+			local jmin = j
+			-- find the end of i
+			repeat
+				j = j + 1
+			until j >= h or self.sandCPU[0 + w * j] ~= color
+			local jmax = j - 1
+			if checkNextCol(1, jmin, jmax, color) then
+			end
+		end
+	end
+	--]]
 end
 
 function App:update(...)
@@ -337,9 +402,8 @@ function App:update(...)
 
 	--[[ pouring sand
 	self.sandCPU[bit.rshift(w,1) + w * (h - 1)] = bit.bor(
-		math.random(0,255),
-		bit.lshift(math.random(0,255),8),
-		bit.lshift(math.random(0,255),16)
+		math.random(0,16777215),
+		0xff000000,
 	)
 	--]]
 
@@ -350,7 +414,7 @@ function App:update(...)
 
 	self.sandTex
 		:bind()
-		:subimage{data=self.sandCPU}
+		:subimage{data=self.sandImage.buffer}
 	local s = w / h
 	gl.glBegin(gl.GL_QUADS)
 	for _,v in ipairs(vtxs) do
