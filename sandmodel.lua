5acea437bc6023c3c3abab1f80c5773ed6d4dc97
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-16 11:34:02 +0700

    added stam realtime-fluid-for-games

--- sandmodel.lua
+++ sandmodel.lua
@@ -285,10 +285,30 @@ CFDSand.name = 'CFD'
 function CFDSand:init(app)
 	CFDSand.super.init(self, app)
 	local w, h = app.sandSize:unpack()
-	self.vel = ffi.new('vec2f_t[?]', w * h)
-	self.nextvel = ffi.new('vec2f_t[?]', w * h)
-	ffi.fill(self.vel, ffi.sizeof'vec2f_t' * w * h)
-	ffi.fill(self.nextvel, ffi.sizeof'vec2f_t' * w * h)
+	local function make()
+		local f = ffi.new('float[?]', w * h)
+		ffi.fill(f, ffi.sizeof'float' * w * h)
+		return f
+	end
+	self.u = make()
+	self.v = make()
+	self.uprev = make()
+	self.vprev = make()
+	--[[ instead of rho and rhoprev I want sandtex ...
+	self.rho = make()
+	self.rhoprev = make()
+	--]]
+	--[[ hmm, how easy would this be to fit into the current framework?
+	self.sandTexPrev = app:makeTexWithImage(app.sandSize)
+	--]]
+	-- [[ I'll try for this way
+	self.r, self.rprev = make(), make()
+	self.g, self.gprev = make(), make()
+	self.b, self.bprev = make(), make()
+	self.a, self.aprev = make(), make()
+	--]]
+	self.div = make()
+	self.p = make()
 end
 function CFDSand:update()
 	local app = self.app
@@ -301,7 +321,8 @@ function CFDSand:update()
 	-- update
 	local sandip = ffi.cast('int32_t*', app.sandTex.image.buffer)
 	local prow = sandip + w
-	local vrow = ffi.cast('vec2f_t*', self.vel) + w
+	local urow = ffi.cast('float*', self.u) + w
+	local vrow = ffi.cast('float*', self.v) + w
 	for j=1,h-1 do
 		-- 50/50 cycling left-to-right vs right-to-left
 		local istart, iend, istep
@@ -311,14 +332,16 @@ function CFDSand:update()
 			istart,iend,istep = w-1, 0, -1
 		end
 		local p = prow + istart
+		local u = urow + istart
 		local v = vrow + istart
 		for i=istart,iend,istep do
 			-- if the cell is blank and there's a sand cell above us ... pull it down
 			if p[0] ~= 0 then
 				if p[-w] == 0 then
 					p[0], p[-w] = p[-w], p[0]
+					u[0], u[-w] = u[-w], u[0]
 					v[0], v[-w] = v[-w], v[0]
-					v[0].y = v[0].y + grav * dt
+					v[0] = v[0] + grav * dt
 					needsCheckLine = true
 				-- hmm symmetry? check left vs right first?
 				elseif app.rng() < app.cfg.toppleChance then
@@ -326,29 +349,33 @@ function CFDSand:update()
 					if app.rng(2) == 2 then
 						if i > 0 and p[-w-1] == 0 then
 							p[0], p[-w-1] = p[-w-1], p[0]
+							u[0], u[-w-1] = u[-w-1], u[0]
 							v[0], v[-w-1] = v[-w-1], v[0]
-							v[0].x = v[0].x - grav * dt
-							v[0].y = v[0].y + grav * dt
+							u[0] = u[0] - grav * dt
+							v[0] = v[0] + grav * dt
 							needsCheckLine = true
 						elseif i < w-1 and p[-w+1] == 0 then
 							p[0], p[-w+1] = p[-w+1], p[0]
+							u[0], u[-w+1] = u[-w+1], u[0]
 							v[0], v[-w+1] = v[-w+1], v[0]
-							v[0].x = v[0].x + grav * dt
-							v[0].y = v[0].y + grav * dt
+							u[0] = u[0] + grav * dt
+							v[0] = v[0] + grav * dt
 							needsCheckLine = true
 						end
 					else
 						if i < w-1 and p[-w+1] == 0 then
 							p[0], p[-w+1] = p[-w+1], p[0]
+							u[0], u[-w+1] = u[-w+1], u[0]
 							v[0], v[-w+1] = v[-w+1], v[0]
-							v[0].x = v[0].x + grav * dt
-							v[0].y = v[0].y + grav * dt
+							u[0] = u[0] + grav * dt
+							v[0] = v[0] + grav * dt
 							needsCheckLine = true
 						elseif i > 0 and p[-w-1] == 0 then
 							p[0], p[-w-1] = p[-w-1], p[0]
+							u[0], u[-w-1] = u[-w-1], u[0]
 							v[0], v[-w-1] = v[-w-1], v[0]
-							v[0].x = v[0].x - grav * dt
-							v[0].y = v[0].y + grav * dt
+							u[0] = u[0] - grav * dt
+							v[0] = v[0] + grav * dt
 							needsCheckLine = true
 						end
 					end
@@ -358,70 +385,222 @@ function CFDSand:update()
 			v = v + istep
 		end
 		prow = prow + w
-		vrow = vrow + w
+		urow = urow + w
 	end
 
-	-- external forces?
+	--[[ external forces?
 	for j=0,h-1 do
 		for i=0,w-1 do
 			local f = i/(w-1)
-			local v = self.vel + (i + w * j)
-			v[0].y = v[0].y + 5 * math.cos(math.pi * f)
+			local v = self.v + (i + w * j)
+			v[0] = v[0] + 5 * math.cos(math.pi * f)
 		end
 	end
+	--]]
 
-	-- advect sand
+	-- [[  copy color to our float buffers
+	-- TODO just use sandTex
 	for j=0,h-1 do
 		for i=0,w-1 do
-			local vx, vy = self.vel[i + w * j]:unpack()
-			local ni = i+.5 - vx
-			local nj = j+.5 - vy
-			ni = math.floor(math.clamp(ni, 0, w-1))
-			nj = math.floor(math.clamp(nj, 0, h-1))
-			if not (ni == i and nj == j) then
---print('advecting sand', i, j)				
-				assert(i >= 0 and i < w and j >= 0 and j < h)
-				-- advect sand
-				sandip[i + w * j],
-				sandip[ni + w * nj]
-				--= 0xffffffff, 0xffffffff
-				=	sandip[i + w * j],
-					sandip[ni + w * nj]		
-			end	
+			self.r[i + w * j] = app.sandTex.image.buffer[0 + 4 * (i + w * j)]
+			self.g[i + w * j] = app.sandTex.image.buffer[1 + 4 * (i + w * j)]
+			self.b[i + w * j] = app.sandTex.image.buffer[2 + 4 * (i + w * j)]
+			self.a[i + w * j] = app.sandTex.image.buffer[3 + 4 * (i + w * j)]
 		end
 	end
-	
-	-- advect velocity
+	--]]
+
+	assert(dt)
+	local visc = .01
+	local diff = .01
+	self:velocityStep(visc, dt)
+	self:densityStep(diff, dt)
+
+	-- [[  copy back
 	for j=0,h-1 do
 		for i=0,w-1 do
-			-- [[ advect
-			local vx, vy = self.vel[i + w * j]:unpack()
-			local ni = i+.5 - vx
-			local nj = j+.5 - vy
-			ni = math.floor(math.clamp(ni, 0, w-1))
-			nj = math.floor(math.clamp(nj, 0, h-1))
-			if not (ni == i and nj == j) then
---print('advecting vel', i, j)				
-				assert(i >= 0 and i < w and j >= 0 and j < h)
-				self.nextvel[i + w * j],
-				self.nextvel[ni + w * nj]
-				=	self.vel[i + w * j],
-					self.vel[ni + w * nj]
-				needsCheckLine = true
-			end
-			--]]
+			app.sandTex.image.buffer[0 + 4 * (i + w * j)] = math.clamp(self.r[i + w * j], 0, 255)
+			app.sandTex.image.buffer[1 + 4 * (i + w * j)] = math.clamp(self.g[i + w * j], 0, 255)
+			app.sandTex.image.buffer[2 + 4 * (i + w * j)] = math.clamp(self.b[i + w * j], 0, 255)
+			app.sandTex.image.buffer[3 + 4 * (i + w * j)] = math.clamp(self.a[i + w * j], 0, 255)
 		end
 	end
-	
-	self.vel, self.nextvel = self.nextvel, self.vel
+	--]]
 
 	return needsCheckLine
+end
+function CFDSand:densityStep(diff, dt)
+	--self:addSource()
+	--[[
+	self.rho, self.rhoprev = self.rhoprev, self.rho
+	self:diffuse(0, self.rho, self.rhoprev, diff, dt)
+	self.rho, self.rhoprev = self.rhoprev, self.rho
+	self:advect(0, self.rho, self.rhoprev, self.u, self.v, dt)
+	--]]
+	-- [[
+	self.r, self.rprev = self.rprev, self.r
+	self:diffuse(0, self.r, self.rprev, diff, dt)
+	self.r, self.rprev = self.rprev, self.r
+	self:advect(0, self.r, self.rprev, self.u, self.v, dt)
+	
+	self.g, self.gprev = self.gprev, self.g
+	self:diffuse(0, self.g, self.gprev, diff, dt)
+	self.g, self.gprev = self.gprev, self.g
+	self:advect(0, self.g, self.gprev, self.u, self.v, dt)
+	
+	self.b, self.bprev = self.bprev, self.b
+	self:diffuse(0, self.b, self.bprev, diff, dt)
+	self.b, self.bprev = self.bprev, self.b
+	self:advect(0, self.b, self.bprev, self.u, self.v, dt)
+	
+	self.a, self.aprev = self.aprev, self.a
+	self:diffuse(0, self.a, self.aprev, diff, dt)
+	self.a, self.aprev = self.aprev, self.a
+	self:advect(0, self.a, self.aprev, self.u, self.v, dt)
+	--]]
+end
+function CFDSand:velocityStep(visc, dt)
+	--self:addSource()
+	self.u, self.uprev = self.uprev, self.u
+	self:diffuse(1, self.u, self.uprev, visc, dt)
+	self.v, self.vprev = self.vprev, self.v
+	self:diffuse(2, self.v, self.vprev, visc, dt)
+	self:projectVel()
+	self.u, self.uprev = self.uprev, self.u
+	self.v, self.vprev = self.vprev, self.v
+	-- advect u0 & v0 into u & v
+	self:advect(1, self.u, self.uprev, self.uprev, self.vprev, dt)
+	self:advect(2, self.v, self.vprev, self.uprev, self.vprev, dt)
+	self:projectVel()
+end
+function CFDSand:diffuse(dir, dst, src, diff, dt)
+	local app = self.app
+	local w, h = app.sandSize:unpack()
+	local dx = 1 --1 / w
+	local dy = 1 --1 / h
+	local dA = dx * dy
+	local a = diff * dt / dA
+	-- Jacobi implementation of Backward-Euler integration...
+	for k=1,20 do
+		for j=1,h-2 do
+			local jR = j+1
+			local jL = j-1
+			for i=1,w-2 do
+				local iR = i+1
+				local iL = i-1
+				dst[i + w * j] = src[i + w * j] + a * (
+					dst[iL + w * j]
+					+ dst[iR + w * j]
+					+ dst[i + w * jL]
+					+ dst[i + w * jR]
+				)
+			end
+		end
+		self:updateBoundary(dir, dst)
+	end
+end
+function CFDSand:advect(dir, dst, src, u, v, dt)
+	local app = self.app
+	local w, h = app.sandSize:unpack()
+	local dx = 1 -- 1/w
+	local dy = 1 -- 1/h
+	local dt_dx = dt / dx
+	local dt_dy = dt / dy
+	for j=1,h-2 do
+		for i=1,w-2 do
+			local x = i - dt_dx * u[i + w * j]
+			x = math.clamp(x, .5, w + .5)
+			local i0 = math.floor(x)
+			local i1 = i0 + 1
+			local s1 = x - i0
+			local s0 = 1 - s1
+			
+			local y = j - dt_dx * v[i + w * j]
+			y = math.clamp(y, .5, h + .5)
+			local j0 = math.floor(y)
+			local j1 = j0 + 1
+			local t1 = y - j0
+			local t0 = 1 - t1	
+		
+			dst[i + w * j] = s0 * (t0 * src[i0 + w * j0] + t1 * src[i0 + w * j1])
+							+ s1 * (t0 * src[i1 + w * j0] + t1 * src[i1 + w * j1])
+		end
+	end
+	self:updateBoundary(dir, dst)
+end
+-- Helmholtz decomposition to make a divergence-free fluid
+function CFDSand:projectVel()
+	local app = self.app
+	local w, h = app.sandSize:unpack()
+	local u = self.u
+	local v = self.v
+	local div = self.div
+	local p = self.p
+	local dx = 1	-- 1/w
+	local dy = 1	-- 1/h
+	for j=1,h-2 do
+		local jR = j+1
+		local jL = j-1
+		for i=1,w-2 do
+			local iR = i+1
+			local iL = i-1
+			div[i + w * j] = -.5 * (
+				dx * (u[iR + w * j] - u[iL + w * j])
+				+ dy * (v[i + w * jR] - v[i + w * jL])
+			)
+			p[i + w * j] = 0
+		end
+	end
+	self:updateBoundary(0, div)
+	self:updateBoundary(0, p)
+
+	for k=1,20 do
+		for j=1,h-2 do
+			local jR = j+1
+			local jL = j-1
+			for i=1,w-2 do
+				local iR = i+1
+				local iL = i-1
+				p[i + w * j] = (div[i + w * j] + p[iL + w * j] + p[iR + w * j] + p[i + w * jL] + p[i + w * jR]) * .25
+			end
+		end
+	end
+	self:updateBoundary(0, p)
+
+	for j=1,h-2 do
+		local jR = j+1
+		local jL = j-1
+		for i=1,w-2 do
+			local iR = i+1
+			local iL = i-1
+			u[i + w * j] = u[i + w * j] - .5 * (p[iR + w * j] - p[iL + w * j]) / dx
+			v[i + w * j] = v[i + w * j] - .5 * (p[i + w * jR] - p[i + w * jL]) / dy
+		end
+	end
+	self:updateBoundary(1, u)
+	self:updateBoundary(2, v)
+end
+function CFDSand:updateBoundary(dir, dst)
+	local app = self.app
+	local w, h = app.sandSize:unpack()
+	for j=1,h-2 do
+		dst[0 + w * j] = dir==1 and -dst[1 + w * j] or dst[1 + w * j]
+		dst[w-1 + w * j] = dir==1 and -dst[w-2 + w * j] or dst[w-2 + w * j]
+	end
+	for i=1,w-2 do
+		dst[i + w * 0] = dir==2 and -dst[i + w * 1] or dst[i + w * 1]
+		dst[i + w * (h-1)] = dir==2 and -dst[i + w * (h-2)] or dst[i + w * (h-2)]
+	end
+	dst[0 + w * 0] = .5 * (dst[1 + w * 0] + dst[0 + w * 1])
+	dst[0 + w * (h-1)] = .5 * (dst[1 + w * (h-1)] + dst[0 + w * (h-2)])
+	dst[w-1 + w * 0] = .5 * (dst[w-2 + w * 0] + dst[w-1 + w * 1])
+	dst[w-1 + w * (h-1)] = .5 * (dst[w-2 + w * (h-1)] + dst[w-1 + w * (h-2)])
 end
 function CFDSand:mergepixel(x,y,color)
 	local app = self.app
 	local w, h = app.sandSize:unpack()
-	local v = self.vel + x + w * y
-	v[0].y = v[0].y - 1
+	local v = self.v + x + w * y
+	v[0] = v[0] - 100
 end
 function CFDSand:clearBlob(blob)
 	local app = self.app
@@ -433,7 +612,8 @@ function CFDSand:clearBlob(blob)
 		ffi.fill(app.sandTex.image.buffer + 4 * (int.x1 + w * int.y), 4 * iw)
 		for k=0,4*iw-1 do
 			app.flashTex.image.buffer[k + 4 * (int.x1 + w * int.y)] = 0xff
-			self.vel[k + 4 * (int.x1 + w * int.y)] = vec2f()
+			self.u[k + 4 * (int.x1 + w * int.y)] = 0
+			self.v[k + 4 * (int.x1 + w * int.y)] = 0
 		end
 	end
 	return clearedCount
@@ -443,13 +623,18 @@ function CFDSand:flipBoard()
 	local w, h = app.sandSize:unpack()
 	local p1 = ffi.cast('int32_t*', app.sandTex.image.buffer)
 	local p2 = p1 + w * h - 1
-	local v1 = ffi.cast('vec2f_t*', self.vel)
+	local u1 = ffi.cast('float*', self.u)
+	local u2 = u1 + w * h - 1
+	local v1 = ffi.cast('float*', self.v)
 	local v2 = v1 + w * h - 1
 	for j=0,bit.rshift(h,1)-1 do
 		for i=0,w-1 do
 			p1[0], p2[0] = p2[0], p1[0]
 			p1 = p1 + 1
 			p2 = p2 - 1
+			u1[0], u2[0] = u2[0], u1[0]
+			u1 = u1 + 1
+			u2 = u2 - 1
 			v1[0], v2[0] = v2[0], v1[0]
 			v1 = v1 + 1
 			v2 = v2 - 1
@@ -460,7 +645,7 @@ end
 SandModel.subclasses = table{
 	AutomataSand,
 	SPHSand,
-	--CFDSand,	-- crashing and doing nothing new
+	CFDSand,
 }
 SandModel.subclassNames = SandModel.subclasses:mapi(function(cl) return cl.name end)
 
