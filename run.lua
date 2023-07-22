dec193417ce73748775197678a38f58c2c6875b6
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-22 12:30:10 +0700

    gles3 too

--- run.lua
+++ run.lua
@@ -3,9 +3,10 @@
 -- hack require packages first:
 
 -- TODO this variable is half hardwired into lua-gl and luajit-ffi-bindings projects ... I don't like how it is set up
-ffi_OpenGL = nil	-- for desktop GL
+--ffi_OpenGL = nil	-- for desktop GL
 --ffi_OpenGL = 'ffi.OpenGLES1'	-- for GLES1 ... but GLES1 has no shaders afaik?
 --ffi_OpenGL = 'ffi.OpenGLES2'	-- for GLES2
+ffi_OpenGL = 'ffi.OpenGLES3'	-- for GLES3
 local gl = require 'gl'
 
 local matrix = require 'matrix.ffi'
