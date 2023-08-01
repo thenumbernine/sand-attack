f0bb694ce44c43d29acf0216556ab8913735f21a
Christopher Moore <christopher.e.moore@gmail.com>
2023-08-01 23:58:07 +0700

    save some memory

--- player.lua
+++ player.lua
@@ -21,8 +21,19 @@ function Player:init(args)
 	self.keyPressLast = {}
 
 	self.pieceTex = app:makeTexWithBlankImage(app.pieceSize)
+	--[[ don't need to hold onto image
+	self.pieceTex.data = nil
+	self.pieceTex.image = nil
+	--]]
+	-- ...nah, still needed by:
+	-- 	- App:updatePieceTex for calculating pieceColMin and pieceColMax
+	--	- SandModel:testPieceMerge and SandModel:mergePiece
+
 	-- give pieces an outline so you can tell players apart
 	self.pieceOutlineTex = app:makeTexWithBlankImage(app.pieceOutlineSize)
+	-- don't need to hold onto image
+	self.pieceOutlineTex.data = nil
+	self.pieceOutlineTex.image = nil
 end
 
 return Player
